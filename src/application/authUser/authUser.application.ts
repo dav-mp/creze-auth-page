9de02c12563831e911a0f0a2407aa09c60e8957d
David Mayo <davidmayopacheco@gmail.com>
2024-11-12 10:36:06 -0600

    Creacion de metodo login en application auth

--- src/application/authUser/authUser.application.ts
+++ src/application/authUser/authUser.application.ts
@@ -1,4 +1,4 @@
-import { UserConfirm, UserRegister } from "../../core/interfaces/authInterfaces.interface";
+import { UserConfirm, UserLogin, UserRegister } from "../../core/interfaces/authInterfaces.interface";
 import { AuthUserUseCase } from "../../core/useCase/authUser.usecase";
 import { AuthService } from "../../services/authService/authService.service";
 
@@ -17,6 +17,14 @@ export const AuthRegisterApplication = async ( user: UserRegister ) => {
 export const UserConfirmApplication = async ( user: UserConfirm ) => {
   try {
     return await authUserUseCase.userConfirm( user )
+  } catch (error) {
+    throw new Error('Error al crear usuario.');
+  }
+}
+
+export const UserLoginApplication = async ( user: UserLogin ) => {
+  try {
+    return await authUserUseCase.authUserLogin( user )
   } catch (error) {
     throw new Error('Error al crear usuario.');
   }
