4503d6f6d6294d8559dccfc3ff32da5fe09b448d
David Mayo <davidmayopacheco@gmail.com>
2024-11-11 22:17:19 -0600

    Creacion de etodo UserConfirmApplication en capa application

--- src/application/authUser/authUser.application.ts
+++ src/application/authUser/authUser.application.ts
@@ -1,4 +1,4 @@
-import { UserRegister } from "../../core/interfaces/authInterfaces.interface";
+import { UserConfirm, UserRegister } from "../../core/interfaces/authInterfaces.interface";
 import { AuthUserUseCase } from "../../core/useCase/authUser.usecase";
 import { AuthService } from "../../services/authService/authService.service";
 
@@ -6,10 +6,18 @@ import { AuthService } from "../../services/authService/authService.service";
 const authService = new AuthService()
 const authUserUseCase = new AuthUserUseCase( authService )
 
-export const AuthLoginApplication = async ( user: UserRegister ) => {
+export const AuthRegisterApplication = async ( user: UserRegister ) => {
   try {
     return await authUserUseCase.userRegister( user )
   } catch (error) {
     throw new Error('Error al crear usuario.');
   }
 };
+
+export const UserConfirmApplication = async ( user: UserConfirm ) => {
+  try {
+    return await authUserUseCase.userConfirm( user )
+  } catch (error) {
+    throw new Error('Error al crear usuario.');
+  }
+}
